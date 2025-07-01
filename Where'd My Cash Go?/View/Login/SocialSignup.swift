063f269f212f8da563000966583445fc6882a746
Shrirang-Zend <college.shrirangzend@gmail.com>
2025-07-02 04:04:56 +0530

    Initial Commit

---
+++ Where'd My Cash Go?/View/Login/SocialSignup.swift
@@ -0,0 +1,18 @@
+//
+//  SocialSignup.swift
+//  Where'd My Cash Go?
+//
+//  Created by Shrirang Zend on 02/07/25.
+//
+
+import SwiftUI
+
+struct SocialSignup: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SocialSignup()
+}
